5f930a479cc4a9376c401b92f269e303c8e42d51
João Victor Mendes <joao.mendes.costa97@gmail.com>
2022-08-10 00:58:15 -0500

    #5 Corrigir erro na operação de divisão

--- calculadora.py
+++ calculadora.py
@@ -13,5 +13,6 @@ print (' {} * {} = '.format(number_1, number_2))
 print (number_1 * number_2)
 
 #division
-print (' {} + {} = '.format(number_1, number_2))
+print (' {} / {} = '.format(number_1, number_2))
 print (number_1 / number_2)
+
